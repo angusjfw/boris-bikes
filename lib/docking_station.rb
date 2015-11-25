b88cf78ae887b51dffb68a6caa7dc1088eefae45
Andrew Burnie <apburnie@hotmail.co.uk>
2015-11-25 12:09:54 +0000

    Progress on stage 11

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,7 +2,15 @@ require_relative 'bike.rb'
 
 class DockingStation
 
+  attr_reader :no_bikes
+
   def release_bike
     Bike.new
   end
+
+def dock(bike)
+
+end
+
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -8,5 +8,11 @@ it "release_bike gets a bike" do
   bike = subject.release_bike
   expect(bike).to be_working
 end
+it "can doc to station" do
+  expect(subject).to respond_to(:dock).with(1).argument
+end
+it "tells us number of bikes" do
+  expect(subject).to respond_to :no_bikes
+end
 
 end
