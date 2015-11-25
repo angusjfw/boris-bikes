9eab34468acfd91833b57409799aad6b0480d930
Andrew Burnie <apburnie@hotmail.co.uk>
2015-11-25 11:04:20 +0000

    stage 10 completed

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,5 +4,9 @@ describe DockingStation do
 it "responds to release_bike" do
   expect(subject).to respond_to :release_bike
 end
+it "release_bike gets a bike" do
+  bike = subject.release_bike
+  expect(bike).to be_working
+end
 
 end
