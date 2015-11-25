b4660c13dabd1a17a50b4bde95d01904e8929902
Andrew Burnie <apburnie@hotmail.co.uk>
2015-11-25 17:51:36 +0000

    Stage 14 complete

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -13,18 +13,18 @@ it "can doc to station" do
   expect(subject).to respond_to(:dock).with(1).argument
 end
 it "responds to bike" do
-  expect(subject).to respond_to :bike
+  expect(subject).to respond_to :bikes
 end
 
 
 describe "#dock" do
   it "docks something" do
     bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
+    expect(subject.dock(bike)).to eq [bike]
   end
 
     it "raises an error when station is full" do
-      subject.dock(Bike.new)
+      20.times {subject.dock(Bike.new)}
       expect{subject.dock(Bike.new)}.to raise_error 'Docking station full'
     end
 
@@ -34,7 +34,7 @@ end
 it "returns docked bikes" do
   bike = Bike.new
   subject.dock(bike)
-  expect(subject.bike).to eq bike
+  expect(subject.bikes).to eq [bike]
 end
 describe "#release_bike" do
   it 'releases a bike' do
