2aaf791907e956db0fef0e23f69776be68524bab
Andrew Burnie <apburnie@hotmail.co.uk>
2015-11-26 15:26:43 +0000

    improved 18

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -3,17 +3,17 @@ require 'bike'
 describe Bike do
 
 describe "#initialize" do
-  it "@working? has default value true" do
+  it "works by default" do
   expect(subject.working?).to eq true
 end
 end
 
-it "Member of public can report bike is broken" do
+it "can be reported broken" do
   subject.broken
   expect(subject).not_to be_working
 end
 
-  it 'Bike responds to working?' do
+  it 'responds to working?' do
     expect(subject).to respond_to :working?
   end
 
